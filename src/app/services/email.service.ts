dfb34dddd12c039984b3fc2fb0a39b2b9b247d17
Nahuel Nuñez <nunezspinelli8@gmail.com>
2024-12-06 23:23:22 -0300

    v4

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -5,7 +5,6 @@ import emailjs from 'emailjs-com';
   providedIn: 'root',
 })
 export class EmailService {
-  // Aquí no necesitas la apiUrl, ya que usaremos el API de EmailJS
   sendEmail(data: any) {
     const templateParams = {
       from_name: data.name,
@@ -13,7 +12,6 @@ export class EmailService {
       message: data.message,
     };
 
-    // Reemplaza con los valores que obtuviste al configurar tu cuenta en EmailJS
     return emailjs.send(
       'Gmail',
       'template_portfolio',
