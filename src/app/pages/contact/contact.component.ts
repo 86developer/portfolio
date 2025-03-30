7cca1d24a2e18dc5341b8ae49a3ea3e2fb3e1c30
Nahuel Nuñez <nunezspinelli8@gmail.com>
2025-03-30 18:12:30 -0300

    email funcional, validaciones

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { EmailService } from '../../services/email.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
@@ -17,15 +18,34 @@ export class ContactComponent {
   constructor(private emailService: EmailService) {}
 
   sendEmail() {
+    // Validar que todos los campos estén llenos
+    if (!this.name || !this.email || !this.message) {
+      // alert('Por favor, completa todos los campos');
+      return;
+    }
+    //Validar el formato del correo electrónico
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // if (!emailRegex.test(this.email)) {
+    //   alert('Por favor, ingresa un correo electrónico válido');
+    //   return;
+    // }
+    //Validar el tamaño del mensaje
+    if (this.message.length < 50) {
+      alert('El mensaje debe tener al menos 50 caracteres');
+      return;
+    }
+    //Llamar al servicio de envío de correo
     this.emailService
       .sendEmail({ name: this.name, email: this.email, message: this.message })
       .then((response) => {
         console.log('Correo enviado con éxito', response);
-        alert('Mensaje enviado');
+        alert('Mensaje enviado!');
       })
       .catch((error) => {
         console.error('Error al enviar correo', error);
-        alert('Error al enviar mensaje');
+        alert(
+          'Error al enviar mensaje. Por favor, inténtalo de nuevo más tarde.'
+        );
       });
   }
 }
--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,3 +1,19 @@
+/*https://www.emailjs.com/
+ * EmailJS is a service that allows you to send emails using JavaScript
+ * without needing a backend server.
+ * It uses a third-party email service (like Gmail, Outlook, etc.) to send emails
+ * directly from your client-side application.
+ * This is useful for sending contact form submissions, notifications, etc.
+ *
+ * duckdev86@gmail.com
+ * password: EmiliaBianca0817
+ *
+ *
+ *
+ * Initialize EmailJS with your user ID
+ * You can find your user ID in the EmailJS dashboard under "Account Settings"
+ */
+
 import { Injectable } from '@angular/core';
 import emailjs from 'emailjs-com';
 
@@ -13,10 +29,19 @@ export class EmailService {
     };
 
     return emailjs.send(
-      'Gmail',
-      'template_portfolio',
+      'service_8ldaf5g', //ID of the service you created in EmailJS
+      'template_portfolio', //ID of the template you created in EmailJS
       templateParams,
-      'JVxXIZ7InzbjWpxvA'
+      'JVxXIZ7InzbjWpxvA' //User ID from EmailJS
     );
+    //Depuracion de errores
+    /*.then((response) => {
+        console.log('Correo enviado con éxito', response);
+        alert('Mensaje enviado');
+      })
+      .catch((error) => {
+        console.error('Error al enviar correo', error);
+        alert('Error al enviar mensaje');
+      });*/
   }
 }
